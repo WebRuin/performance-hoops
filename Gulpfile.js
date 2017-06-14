5c4d5849ad47fe16e7ef9be9a4540cbe7345b7b5
Tim Smith <tim.smith.hdg@gmail.com>
2017-06-13 22:57:42 -0700

    min-css

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('styles', function() {
-    gulp.src('assets/sass/**/*.scss')
+    gulp.src('./assets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./assets/css/'))
 });
 
+gulp.task('minify-css', function() {
+  return gulp.src('./assets/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./assets/css/'));
+});
+
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('assets/sass/**/*.scss',['styles']);
+    gulp.watch('./assets/sass/**/*.scss',['styles']);
 });
