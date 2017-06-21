2dae0a1d5d6b3c2f23cc424ca84a4739c98670c3
Tim Smith <tim.smith.hdg@gmail.com>
2017-06-21 12:11:59 -0700

    min

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,13 +5,25 @@ var uglify = require('gulp-uglify');
 var purify = require('gulp-purifycss');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('styles', function() {
+gulp.task('css-clean', function() {
+  return gulp.src('./assets/css/main.css')
+    .pipe(purify(['./assets/js/**/*.js', './**/*.html']))
+    .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('sass', function() {
     gulp.src('./assets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./assets/css/'))
 });
 
-gulp.task('js-min', function (cb) {
+gulp.task('minify-css', function() {
+  return gulp.src('./assets/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('minify-js', function (cb) {
   pump([
         gulp.src('./assets/js/**/*.js'),
         uglify(),
@@ -21,19 +33,9 @@ gulp.task('js-min', function (cb) {
   );
 });
 
-gulp.task('css-clean', function() {
-  return gulp.src('./assets/css/main.css')
-    .pipe(purify(['./assets/js/**/*.js', './**/*.html']))
-    .pipe(gulp.dest('./assets/css/'));
-});
-
-gulp.task('minify-css', function() {
-  return gulp.src('./assets/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./assets/css/'));
-});
-
 //Watch task
 gulp.task('default',function() {
     gulp.watch('./assets/sass/**/*.scss',['styles']);
 });
+
+gulp.task('prod', ['css-clean', 'sass', 'minify-css', 'minify-js']);
